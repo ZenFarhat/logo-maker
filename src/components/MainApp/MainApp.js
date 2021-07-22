a378473b39c7310bd028028f0c6c05963220e86e
Zen F <zeinfarhat6@gmail.com>
2021-07-22 20:20:51 +1000

    drag n drop semi working

--- src/components/MainApp/MainApp.js
+++ src/components/MainApp/MainApp.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './MainApp.css'
 
 function MainApp() {
-    return (
-        <div className="mainApp">
-            <div className="canvas"></div>
-        </div>
-    )
+  const [coordinates, setCoordinates] = useState([0, 0])
+
+  useEffect(() => {}, [coordinates])
+
+  const dragEl = (e) => {
+    let x = e.pageX
+    let y = e.pageY
+    setCoordinates([x, y])
+  }
+
+  return (
+    <div className='mainApp'>
+      <div className='canvas'>
+        <div
+          draggable={true}
+          onDragEnd={dragEl}
+          className='circle'
+          style={{ left: coordinates[0], top: coordinates[1] }}
+          id='circle'
+        ></div>
+      </div>
+    </div>
+  )
 }
 
 export default MainApp
